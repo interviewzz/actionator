91fc755d4e9b14a471fdc56bbd4200984c04cc7d
K.P <katlyn333+interviews@gmail.com>
2019-08-10 15:55:47 -0600

    added a sample script showing multithreading usage of the actionator

--- README.md
+++ README.md
@@ -14,6 +14,16 @@ $ python3 --version
 Python 3.7.4
 ```
 
+### Usage
+You can see an example of how to use the actionator in a multi-threaded environment
+in [sample_multithreading.py](https://github.com/interviewzz/actionator/blob/master/sample_multithreading.py)
+
+```
+$ python3 sample_multithreading.py
+Expect the average time for the action jump to be 50
+[{"action": "jump", "avg": 50.0}]
+```
+
 ### Tests
 Run unit tests for the actionator
 
---
+++ sample_multithreading.py
@@ -0,0 +1,23 @@
+from actionator import addAction, getStats
+import threading
+import json
+
+def thread_task():
+    for i in range(1, 100):
+        addAction(json.dumps({"action": "jump", "time": i}))
+
+thread1 = threading.Thread(target=thread_task)
+thread2 = threading.Thread(target=thread_task)
+
+# Kickoff threads
+thread1.start()
+thread2.start()
+
+# Wait until all threads are done
+thread1.join()
+thread2.join()
+
+result = getStats()
+assert result == '[{"action": "jump", "avg": 50.0}]'
+
+print(f"Expect the average time for the action jump to be 50\n{result}")
